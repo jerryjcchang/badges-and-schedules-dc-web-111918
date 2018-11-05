c3d2a315b4a52c620b5fb417bae2e3d96026b2c5
JC Chang <jerryjcchang@gmail.com>
2018-11-05 23:55:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+  badge = []
+  list.each do |name|
+    badge << "Hello, my name is #{name}."
+  end
+  badge
+end
+
+def assign_rooms(list)
+end
