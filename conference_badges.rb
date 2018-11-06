21df09a68aef76ad7784eee2d931115cff9e36ed
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:09:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(room_list)
   room_assign = []
     room_list.each_with_index{ |name, index| }
-    room_assign = "Hello, #{name}! You'll be assigned to room #{index-1}!"
+    room_assign = "Hello, #{name}! You'll be assigned to room #{index}!"
   end
   room_assign
 end
