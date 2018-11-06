62aa09c2186746372c64dc95899a4e5fb047ec7c
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:20:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,8 @@ end
 
 def printer(attendees)
   badges = batch_badge_creator(attendees)
-  badges.each do |label|
-    puts label 
+  badges.each do |name_badge|
+    puts name_badge 
   end
   room_list = assign_rooms(attendees)
   room_list.each do |room|
